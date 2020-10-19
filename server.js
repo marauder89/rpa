1548875a7f659a72c630b8f00c8f0771e383de03
김동훈 <marauder89@pcninc.co.kr>
2020-10-19 13:14:04 +0900

    ip 설정 추가

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/getUrl', async(req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
